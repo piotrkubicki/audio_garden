95349ae54d7ab8c30b6edab7698990b27158da1e
Piotr Kubicki <pkubicki44@gmail.com>
2017-02-25 12:38:00 +0000

    test db insert

--- server/db_create.py
+++ server/db_create.py
@@ -9,10 +9,13 @@ from app import Location
 from app import Transmitter
 
 l = Location('Audio Garden', '1', '1')
-t1 = Transmitter(1, '1.mp3', '1.mp3', 1)
-t2 = Transmitter(2, '2.mp3', '2.mp3', 1)
+l2 = Location('Dummy Place', '3', '5')
+t1 = Transmitter('00:1A:7D:DA:71:11', '1.mp3', '1.mp3', 1)
+#t2 = Transmitter('0000fe9f-0000-1000-8000-00805f9b34fb', '2.mp3', '2.mp3', 1)
+t2 = Transmitter('54:60:09:42:4F:05', '2.mp3', '2.mp3', 1)
 
 db.session.add(l)
 db.session.add(t1)
 db.session.add(t2)
+db.session.add(l2)
 db.session.commit()
